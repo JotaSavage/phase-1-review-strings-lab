28737fdefbb9a6d2ab2bbaae9ad63873fc42bba5
Juan Lugo <juanlugodev@gmail.com>
2023-08-21 13:46:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'JUAN'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+let shortGreeting = `Welcome, J!`
